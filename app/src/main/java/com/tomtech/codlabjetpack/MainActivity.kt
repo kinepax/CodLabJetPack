b423c37d1445f39790d2bd709381246a9f427297
Paulo <kinepax@hotmail.com>
2025-03-28 10:01:51 -0500

    [feat] elevacion de estado

--- app/src/main/java/com/tomtech/codlabjetpack/MainActivity.kt
+++ app/src/main/java/com/tomtech/codlabjetpack/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,8 +17,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,24 +43,62 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(
-    modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    modifier: Modifier = Modifier
 ) {
-    Column(modifier) {
-        for (name in names) {
-            Greeting(name = name)
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    Surface(modifier) {
+        if(shouldShowOnboarding){
+            OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
+        }
+        else{
+            Greetings()
+        }
+    }
+
+}
+
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    // TODO: This state should be hoisted
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick =  onContinueClicked
+        ) {
+            Text("Continue")
         }
     }
 }
 
 
 @Composable
+fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val expanded = remember {
+    var  expanded by  remember {
         mutableStateOf(false)
     }
 
-    val extraPadding = if(expanded.value) 48.dp else 0.dp
+    val extraPadding = if (expanded) 48.dp else 0.dp
 
     Surface(
         color = MaterialTheme.colorScheme.primary,
@@ -65,15 +107,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
                 modifier = modifier
-                    .weight(1f).padding(bottom = extraPadding)
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello")
                 Text(text = name)
 
             }
 
-            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(if (expanded.value) "Show less" else "Show More")
+            ElevatedButton(onClick = { expanded = !expanded }) {
+                Text(if (expanded) "Show less" else "Show More")
             }
         }
 
@@ -86,6 +129,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     CodLabJetPackTheme {
-        MyApp()
+        MyApp(modifier = Modifier.fillMaxSize())
     }
 }
+
